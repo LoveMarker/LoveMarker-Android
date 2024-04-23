b8ee621fc758ad3c210855b1882469eedd39bba2
leeeha <jxlhe46@gmail.com>
2024-04-23 20:49:20 +0900

    [add] google places api 키 추가

--- app/src/main/java/com/capstone/lovemarker/MainActivity.kt
+++ app/src/main/java/com/capstone/lovemarker/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.capstone.lovemarker
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,10 +12,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.capstone.lovemarker.ui.theme.LoveMarkerTheme
+import com.google.android.libraries.places.api.Places
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val apiKey = BuildConfig.PLACES_API_KEY
+        Log.d("API_KEY", apiKey)
+        Places.initializeWithNewPlacesApiEnabled(applicationContext, apiKey)
+        val placesClient = Places.createClient(this)
+
         setContent {
             LoveMarkerTheme {
                 // A surface container using the 'background' color from the theme
