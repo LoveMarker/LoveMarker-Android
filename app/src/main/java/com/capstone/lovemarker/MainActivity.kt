266e37961025a5e217847daf4f5a00c134364031
leeeha <jxlhe46@gmail.com>
2024-04-30 23:47:20 +0900

    [feat] State

--- app/src/main/java/com/capstone/lovemarker/MainActivity.kt
+++ app/src/main/java/com/capstone/lovemarker/MainActivity.kt
@@ -3,71 +3,82 @@ package com.capstone.lovemarker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarDuration
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val viewModel = viewModel<MainViewModel>()
 
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Text(
-                    text = viewModel.data.value,
-                    fontSize = 30.sp,
-                )
-                Spacer(modifier = Modifier.padding(12.dp))
-                Button(onClick = {
-                    viewModel.updateText("World")
-                }) {
-                    Text("변경")
-                }
-            }
         }
     }
 }
 
-class MainViewModel: ViewModel() {
-    private val _data = mutableStateOf("Hello")
-    val data: State<String> = _data // 읽기 전용 state
+@Composable
+fun HomeScreen(viewModel: MainViewModel = viewModel()) {
+    // state 변할 때마다 recomposition 발생
+    val text1: MutableState<String> = remember {
+        mutableStateOf("Hello")
+    }
 
-    // 데이터는 UI가 아니라 뷰모델에서 수정한다. (외부에서 함부로 조작하지 못하도록)
-    fun updateText(value: String) {
-        _data.value = value
+    // by -> delegate pattern (getter, setter 재정의)
+    var text2: String by remember {
+        mutableStateOf("World")
+    }
+
+    // Destructuring -> TextField
+    val (text: String, setText: (String) -> Unit) = remember {
+        mutableStateOf("Hello World")
+    }
+
+    Column {
+        Text("Hello World")
+        Button(onClick = {
+            text1.value = "변경 1"
+            print(text1.value)
+
+            text2 = "변경 2"
+            print(text2)
+
+            setText("변경 3")
+
+            viewModel.updateValue("변경 4")
+        }) {
+            Text("클릭")
+        }
+        TextField(value = text, onValueChange = setText)
+    }
+}
+
+class MainViewModel : ViewModel() {
+    private val _state = mutableStateOf("Hello")
+    val state: State<String> = _state
+
+    private val _liveData = MutableLiveData<String>()
+    val liveData: LiveData<String> = _liveData
+
+    private val _flow = MutableStateFlow("Hello")
+    val flow: StateFlow<String> = _flow.asStateFlow()
+
+    fun updateValue(value: String) {
+        _state.value = value
     }
 }
