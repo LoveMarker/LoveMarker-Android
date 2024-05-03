ff7000a7525605aa6e414361f500ba39edf00e92
leeeha <jxlhe46@gmail.com>
2024-05-03 22:43:12 +0900

    [feat] UI와 로직 연결

--- app/src/main/java/com/capstone/lovemarker/MainActivity.kt
+++ app/src/main/java/com/capstone/lovemarker/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.capstone.lovemarker
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -21,9 +18,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -34,8 +29,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.capstone.lovemarker.ui.theme.LoveMarkerTheme
-import com.google.android.libraries.places.api.Places
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,7 +38,28 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             LoveMarkerTheme {
-                MainPreview()
+                val viewModel = viewModel<MainViewModel>()
+
+                val sec = viewModel.sec.value
+                val millis = viewModel.millis.value
+                val isRunning = viewModel.isRunning.value
+                val lapTimes = viewModel.lapTimes.value
+
+                MainScreen(
+                    sec = sec,
+                    millis = millis,
+                    isRunning = isRunning,
+                    lapTimes = lapTimes,
+                    onReset = { viewModel.reset() },
+                    onToggle = { running ->
+                        if (running) {
+                            viewModel.pause()
+                        } else {
+                            viewModel.start()
+                        }
+                    },
+                    onLapTime = { viewModel.recordLapTime() }
+                )
             }
         }
     }
@@ -76,6 +92,7 @@ fun MainScreen(
             Spacer(modifier = Modifier.height(40.dp))
 
             Row(
+                verticalAlignment = Alignment.Bottom
             ) {
                 Text("$sec", fontSize = 100.sp)
                 Text("$millis")
@@ -101,8 +118,8 @@ fun MainScreen(
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 FloatingActionButton(
-                    modifier = Modifier.background(color = Color.Red),
                     onClick = { onReset() },
+                    containerColor = Color.Red
                 ) {
                     Image(
                         painter = painterResource(id = R.drawable.ic_refresh_24),
@@ -111,11 +128,12 @@ fun MainScreen(
                 }
 
                 FloatingActionButton(
-                    modifier = Modifier.background(color = Color.Green),
                     onClick = { onToggle(isRunning) },
+                    containerColor = Color.Green
                 ) {
                     Image(
-                        painter = painterResource(id =
+                        painter = painterResource(
+                            id =
                             if (isRunning) R.drawable.ic_pause_24
                             else R.drawable.ic_play_arrow_24
                         ),
