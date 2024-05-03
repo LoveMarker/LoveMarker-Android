17daeb886fc78b8c374a86daecee690bf135bf58
leeeha <jxlhe46@gmail.com>
2024-05-03 23:02:48 +0900

    [del] 컴포즈 기본 코드 삭제

--- app/src/main/java/com/capstone/lovemarker/MainActivity.kt
+++ app/src/main/java/com/capstone/lovemarker/MainActivity.kt
@@ -1,54 +1,18 @@
 package com.capstone.lovemarker
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.capstone.lovemarker.ui.theme.LoveMarkerTheme
-import com.google.android.libraries.places.api.Places
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val apiKey = BuildConfig.PLACES_API_KEY
-        Log.d("API_KEY", apiKey)
-        Places.initializeWithNewPlacesApiEnabled(applicationContext, apiKey)
-        val placesClient = Places.createClient(this)
-
         setContent {
             LoveMarkerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LoveMarkerTheme {
-        Greeting("Android")
     }
 }
