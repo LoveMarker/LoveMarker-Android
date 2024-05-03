5a0b8dfa84bf179b7657943812daa1660dfd4ca5
leeeha <jxlhe46@gmail.com>
2024-05-03 21:29:43 +0900

    [feat] 타이머 시작, 종료, 리셋, lap time 기록 기능 구현

--- app/src/main/java/com/capstone/lovemarker/MainActivity.kt
+++ app/src/main/java/com/capstone/lovemarker/MainActivity.kt
@@ -18,37 +18,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val apiKey = BuildConfig.PLACES_API_KEY
-        Log.d("API_KEY", apiKey)
-        Places.initializeWithNewPlacesApiEnabled(applicationContext, apiKey)
-        val placesClient = Places.createClient(this)
-
         setContent {
             LoveMarkerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LoveMarkerTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/capstone/lovemarker/MainViewModel.kt
@@ -0,0 +1,57 @@
+package com.capstone.lovemarker
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import java.util.Timer
+import kotlin.concurrent.timer
+
+class MainViewModel: ViewModel() {
+    private var time = 0
+    private var lap = 1
+    private var timerTask: Timer? = null
+
+    private val _isRunning = mutableStateOf(false)
+    val isRunning : State<Boolean> = _isRunning
+
+    private val _sec = mutableStateOf(0)
+    val sec : State<Int> = _sec
+
+    private val _millis = mutableStateOf(0)
+    val millis : State<Int> = _millis
+
+    private val _lapTimes = mutableStateOf(mutableListOf<String>())
+    val lapTimes: State<List<String>> = _lapTimes
+
+    fun start() {
+        _isRunning.value = true
+
+        timerTask = timer(period = 10) {
+            time++
+            _sec.value = time / 100
+            _millis.value = time % 100
+        }
+    }
+
+    fun pause() {
+        _isRunning.value = false
+        timerTask?.cancel()
+    }
+
+    fun reset() {
+        timerTask?.cancel()
+
+        time = 0
+        lap = 0
+        _lapTimes.value.clear()
+
+        _isRunning.value = false
+        _sec.value = 0
+        _millis.value = 0
+    }
+
+    fun recordLapTime() {
+        _lapTimes.value.add(0, "$lap LAP : ${sec.value} ${millis.value}")
+        lap++
+    }
+}
