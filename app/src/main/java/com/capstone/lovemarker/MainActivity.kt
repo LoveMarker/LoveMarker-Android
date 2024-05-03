89b26ac5a4703f0273e3b45025966555ab447558
leeeha <jxlhe46@gmail.com>
2024-05-03 22:34:05 +0900

    [ui] 타이머 UI 구현

--- app/src/main/java/com/capstone/lovemarker/MainActivity.kt
+++ app/src/main/java/com/capstone/lovemarker/MainActivity.kt
@@ -1,16 +1,39 @@
 package com.capstone.lovemarker
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.capstone.lovemarker.ui.theme.LoveMarkerTheme
 import com.google.android.libraries.places.api.Places
 
@@ -20,8 +43,98 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             LoveMarkerTheme {
-
+                MainPreview()
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(
+    sec: Int,
+    millis: Int,
+    isRunning: Boolean,
+    lapTimes: List<String>,
+    onReset: () -> Unit,
+    onToggle: (Boolean) -> Unit,
+    onLapTime: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                Text("Stop Watch")
+            })
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Spacer(modifier = Modifier.height(40.dp))
+
+            Row(
+            ) {
+                Text("$sec", fontSize = 100.sp)
+                Text("$millis")
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Column(
+                modifier = Modifier
+                    .weight(1F) // 수직 방향으로 남은 영역 모두 차지
+                    .verticalScroll(rememberScrollState())
+            ) {
+                lapTimes.forEach { time ->
+                    Text(time)
+                }
+            }
+
+            Row(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                FloatingActionButton(
+                    modifier = Modifier.background(color = Color.Red),
+                    onClick = { onReset() },
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_refresh_24),
+                        contentDescription = "reset"
+                    )
+                }
+
+                FloatingActionButton(
+                    modifier = Modifier.background(color = Color.Green),
+                    onClick = { onToggle(isRunning) },
+                ) {
+                    Image(
+                        painter = painterResource(id =
+                            if (isRunning) R.drawable.ic_pause_24
+                            else R.drawable.ic_play_arrow_24
+                        ),
+                        contentDescription = "start or pause"
+                    )
+                }
+
+                Button(
+                    onClick = { onLapTime() }
+                ) {
+                    Text("랩 타임")
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MainPreview(modifier: Modifier = Modifier) {
+
 }
