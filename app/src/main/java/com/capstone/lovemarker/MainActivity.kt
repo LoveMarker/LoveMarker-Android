97e595bb9e1d6946f4c78d313846d81108079206
leeeha <jxlhe46@gmail.com>
2024-05-04 21:35:57 +0900

    [feat] 이전, 다음 url이 없으면 스낵바 띄우기

--- app/src/main/java/com/capstone/lovemarker/MainActivity.kt
+++ app/src/main/java/com/capstone/lovemarker/MainActivity.kt
@@ -16,27 +16,32 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.capstone.lovemarker.ui.theme.LoveMarkerTheme
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,15 +66,17 @@ fun HomeScreen(
     }
 
     val focusManager = LocalFocusManager.current
-    val keyboardController = LocalSoftwareKeyboardController.current
+
+    val snackbarHostState = remember { SnackbarHostState() }
 
     Scaffold(
+        snackbarHost = { SnackbarHost(snackbarHostState) },
         topBar = {
             TopAppBar(
                 title = { Text("나만의 웹 브라우저") },
                 actions = {
                     IconButton(onClick = {
-
+                        viewModel.undo()
                     }) {
                         Icon(
                             imageVector = Icons.AutoMirrored.Filled.ArrowBack,
@@ -78,7 +85,7 @@ fun HomeScreen(
                         )
                     }
                     IconButton(onClick = {
-
+                        viewModel.redo()
                     }) {
                         Icon(
                             imageVector = Icons.AutoMirrored.Filled.ArrowForward,
@@ -110,7 +117,10 @@ fun HomeScreen(
                     })
             )
             Spacer(modifier = Modifier.height(16.dp))
-            MyWebView(viewModel)
+            MyWebView(
+                viewModel = viewModel,
+                snackbarHostState = snackbarHostState
+            )
         }
     }
 }
@@ -118,18 +128,51 @@ fun HomeScreen(
 @Composable
 fun MyWebView(
     viewModel: MainViewModel,
+    snackbarHostState: SnackbarHostState,
 ) {
+    val webView = rememberWebView()
+
+    // LaunchedEffect의 key 값이 바뀐 경우에만
+    // suspend 함수의 동작을 취소하고 재실행시킨다.
+    // key: true, Unit -> suspend 함수 한번만 실행
+    LaunchedEffect(Unit) {
+        viewModel.undoSharedFlow.collectLatest {
+            if (webView.canGoBack()) {
+                webView.goBack()
+            } else {
+                snackbarHostState.showSnackbar("더 이상 뒤로 갈 수 없음")
+            }
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        viewModel.redoSharedFlow.collectLatest {
+            if (webView.canGoForward()) {
+                webView.goForward()
+            } else {
+                snackbarHostState.showSnackbar("더 이상 앞으로 갈 수 없음")
+            }
+        }
+    }
+
     AndroidView(
         modifier = Modifier.fillMaxSize(),
-        factory = {
-            WebView(it).apply {
-                settings.javaScriptEnabled = true
-                webViewClient = WebViewClient()
-                loadUrl("https://www.google.com")
-            }
-        },
-        update = { webView ->
-            webView.loadUrl(viewModel.url.value)
+        factory = { webView },
+        update = { wb ->
+            wb.loadUrl(viewModel.url.value)
         }
     )
+}
+
+@Composable
+fun rememberWebView(): WebView {
+    val context = LocalContext.current
+    val webView = remember {
+        WebView(context).apply {
+            settings.javaScriptEnabled = true
+            webViewClient = WebViewClient()
+            loadUrl("https://www.google.com")
+        }
+    }
+    return webView
 }
