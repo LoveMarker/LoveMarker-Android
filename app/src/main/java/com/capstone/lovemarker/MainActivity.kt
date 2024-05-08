8eeac057ce549777d150cfaf1b92fc6bef8cb3e3
leeeha <jxlhe46@gmail.com>
2024-05-09 02:56:37 +0900

    [feat] 이미지 선택하면 다음 화면에서 페이저로 표시

--- app/src/main/java/com/capstone/lovemarker/MainActivity.kt
+++ app/src/main/java/com/capstone/lovemarker/MainActivity.kt
@@ -4,7 +4,6 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.ext.SdkExtensions.getExtensionVersion
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.PickVisualMediaRequest
@@ -17,6 +16,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -25,28 +25,47 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import coil.compose.rememberAsyncImagePainter
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     private val viewModel by viewModels<MainViewModel>()
     private val singlePicker =
         registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri -> }
+
     private val multiplePicker =
         registerForActivityResult(ActivityResultContracts.PickMultipleVisualMedia(MAX_ITEMS)) { uris ->
             if (uris.isNotEmpty()) {
                 viewModel.fetchPhotos(uris)
             }
         }
+
     private val imageContentLauncher =
         registerForActivityResult(ActivityResultContracts.GetContent()) { uri -> }
 
@@ -54,16 +73,31 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            if (viewModel.photoUris.value.isEmpty()) {
-                HomeScreen {
-                    if (isPhotoPickerAvailable()) {
-                        launchMultiplePhotoPicker()
-                    } else {
-                        launchGallery()
-                    }
+            val navController = rememberNavController()
+
+            NavHost(
+                navController = navController,
+                startDestination = "home",
+            ) {
+                composable(route = "home") {
+                    HomeScreen(
+                        navController = navController,
+                        viewModel = viewModel,
+                        onPickButtonClick = {
+                            if (isPhotoPickerAvailable()) {
+                                launchMultiplePhotoPicker()
+                            } else {
+                                launchGallery()
+                            }
+                        }
+                    )
                 }
-            } else {
-                PhotoScreen(photoUris = viewModel.photoUris.value)
+                composable(route = "photo") {
+                    PhotoScreen(
+                        navController = navController,
+                        photoUris = viewModel.photoUris.value
+                    )
+                }
             }
         }
     }
@@ -96,68 +130,125 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun HomeScreen(onClick: () -> Unit) {
-    Box(
-        modifier = Modifier
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Button(
-            onClick = onClick,
+fun HomeScreen(
+    navController: NavController,
+    viewModel: MainViewModel,
+    onPickButtonClick: () -> Unit,
+) {
+    val snackBarHostState = remember {
+        SnackbarHostState()
+    }
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackBarHostState) },
+    ) { innerPadding ->
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp)
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("이미지 고르기")
+            Button(
+                onClick = onPickButtonClick,
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                Text("이미지 고르기")
+            }
+            Spacer(modifier = Modifier.padding(8.dp))
+            Button(
+                onClick = {
+                    if (viewModel.photoUris.value.isNotEmpty()) {
+                        navController.navigate("photo")
+                    } else {
+                        coroutineScope.launch {
+                            snackBarHostState.showSnackbar(
+                                 message = "이미지를 선택해주세요",
+                                duration = SnackbarDuration.Short
+                            )
+                        }
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("다음으로 이동")
+            }
         }
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun PhotoScreen(
+    navController: NavController,
     photoUris: List<Uri>,
 ) {
     val pagerState = rememberPagerState(pageCount = { photoUris.size })
 
-    Column(Modifier.fillMaxSize()) {
-        HorizontalPager(
-            state = pagerState,
-            modifier = Modifier
-                .weight(1F)
-                .padding(16.dp)
-                .fillMaxSize()
-        ) { pageIndex ->
-            Card() {
-                Image(
-                    painter = rememberAsyncImagePainter(
-                        model = photoUris[pageIndex]
-                    ),
-                    contentDescription = null,
-                    modifier = Modifier.fillMaxSize(),
-                    contentScale = ContentScale.Crop
-                )
-            }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {},
+                navigationIcon = {
+                    IconButton(
+                        modifier = Modifier.padding(6.dp),
+                        onClick = {
+                            navController.navigateUp()
+                        }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "back button"
+                        )
+                    }
+                },
+            )
         }
-
-        Row(
-            modifier = Modifier
-                .wrapContentHeight()
-                .fillMaxWidth()
-                .align(Alignment.CenterHorizontally)
-                .padding(bottom = 16.dp),
-            horizontalArrangement = Arrangement.Center
+    ) { innerPadding ->
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
         ) {
-            repeat(pagerState.pageCount) { iteration ->
-                val color =
-                    if (pagerState.currentPage == iteration) Color.DarkGray else Color.LightGray
-                Box(
-                    modifier = Modifier
-                        .padding(2.dp)
-                        .size(8.dp)
-                        .clip(CircleShape)
-                        .background(color)
-                )
+            HorizontalPager(
+                state = pagerState,
+                modifier = Modifier
+                    .weight(1F)
+                    .padding(16.dp)
+                    .fillMaxSize()
+            ) { pageIndex ->
+                Card() {
+                    Image(
+                        painter = rememberAsyncImagePainter(
+                            model = photoUris[pageIndex]
+                        ),
+                        contentDescription = null,
+                        modifier = Modifier.fillMaxSize(),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+            }
+
+            Row(
+                modifier = Modifier
+                    .wrapContentHeight()
+                    .fillMaxWidth()
+                    .align(Alignment.CenterHorizontally)
+                    .padding(bottom = 16.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                repeat(pagerState.pageCount) { iteration ->
+                    val color =
+                        if (pagerState.currentPage == iteration) Color.DarkGray else Color.LightGray
+                    Box(
+                        modifier = Modifier
+                            .padding(2.dp)
+                            .size(8.dp)
+                            .clip(CircleShape)
+                            .background(color)
+                    )
+                }
             }
         }
     }
