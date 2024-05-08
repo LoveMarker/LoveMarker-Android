a21d854a26c2c5f69c20acdf8ef97518f3958a1a
leeeha <jxlhe46@gmail.com>
2024-05-09 00:06:25 +0900

    [feat] Photo Picker 이용하여 갤러리 이미지 가져오기

--- app/src/main/java/com/capstone/lovemarker/MainActivity.kt
+++ app/src/main/java/com/capstone/lovemarker/MainActivity.kt
@@ -1,18 +1,58 @@
 package com.capstone.lovemarker
 
+import android.os.Build
 import android.os.Bundle
+import android.os.ext.SdkExtensions.getExtensionVersion
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.capstone.lovemarker.ui.theme.LoveMarkerTheme
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            LoveMarkerTheme {
-
+            if (isPhotoPickerAvailable()) {
+                launchPhotoPicker()
+            } else {
+                launchGallery()
             }
         }
+    }
+
+    private fun isPhotoPickerAvailable(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            true
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            getExtensionVersion(Build.VERSION_CODES.R) >= 2
+        } else {
+            false
+        }
+    }
+
+    private fun launchPhotoPicker() {
+        val pickMedia =
+            registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+                if (uri != null) {
+
+                } else {
+
+                }
+            }
+
+        pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+
+    private fun launchGallery() {
+        val launcher =
+            registerForActivityResult(ActivityResultContracts.GetContent()) { imageUri ->
+                if (imageUri != null) {
+
+                }
+            }
+
+        launcher.launch("image/*")
     }
 }
