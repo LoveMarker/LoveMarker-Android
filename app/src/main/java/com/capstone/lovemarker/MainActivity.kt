88d6c18cbd45e26ed8684ed89a2b2ccbf2b22162
leeeha <jxlhe46@gmail.com>
2024-05-09 16:07:31 +0900

    [feat] 실로폰 UI, 사운드 재생 기능 구현

--- app/src/main/java/com/capstone/lovemarker/MainActivity.kt
+++ app/src/main/java/com/capstone/lovemarker/MainActivity.kt
@@ -1,18 +1,94 @@
 package com.capstone.lovemarker
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.capstone.lovemarker.ui.theme.LoveMarkerTheme
 
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
 
         setContent {
             LoveMarkerTheme {
-
+                XylophoneScreen(viewModel)
             }
         }
+    }
+}
+
+@Composable
+fun XylophoneScreen(
+    viewModel: MainViewModel,
+) {
+    val keys = listOf(
+        Pair("도", Color.Red),
+        Pair("레", Color(0xFFFF9800)),
+        Pair("미", Color(0xFFFFC107)),
+        Pair("파", Color(0xFF8BC34A)),
+        Pair("솔", Color(0xFF2196F3)),
+        Pair("라", Color(0xFF3F51B5)),
+        Pair("시", Color(0xFF673AB7)),
+        Pair("도", Color.Red),
+    )
+
+    Row(
+        modifier = Modifier.fillMaxSize(),
+        horizontalArrangement = Arrangement.SpaceEvenly // 모든 요소 간의 간격 동일
+    ) {
+        keys.forEachIndexed { index, key ->
+            val padding = (index + 2) * 8 // 16 24 32 ..
+            Keyboard(
+                modifier = Modifier
+                    .padding(top = padding.dp, bottom = padding.dp)
+                    .clickable {
+                        viewModel.playSound(index)
+                    },
+                text = key.first,
+                color = key.second
+            )
+        }
+    }
+}
+
+@Composable
+fun Keyboard(
+    modifier: Modifier,
+    text: String,
+    color: Color,
+) {
+    Box(
+        modifier = modifier
+            .width(50.dp)
+            .fillMaxHeight()
+            .background(color = color)
+    ) {
+        Text(
+            text = text,
+            style = TextStyle(color = Color.White, fontSize = 20.sp),
+            modifier = Modifier.align(Alignment.Center),
+        )
     }
 }
