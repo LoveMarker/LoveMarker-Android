1a25cddd2912aa5da368135f79c03df49a6e0853
leeeha <jxlhe46@gmail.com>
2024-05-13 19:08:20 +0900

    [feat] 구글 MapView 띄우는 기본 코드 작성

--- app/src/main/java/com/capstone/lovemarker/MainActivity.kt
+++ app/src/main/java/com/capstone/lovemarker/MainActivity.kt
@@ -3,7 +3,19 @@ package com.capstone.lovemarker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import com.capstone.lovemarker.ui.theme.LoveMarkerTheme
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +23,61 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             LoveMarkerTheme {
-
+                MyMapView()
             }
         }
     }
+}
+
+
+
+@Composable
+fun MyMapView() {
+    val map = rememberMapView()
+    AndroidView(
+        factory = { map },
+        update = { mapView ->
+            mapView.getMapAsync  { googleMap ->
+                val sydney = LatLng(-34.0, 151.0)
+                googleMap.addMarker(
+                    MarkerOptions().position(sydney).title("Marker in Sydney")
+                )
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+            }
+        }
+    )
+}
+
+@Composable
+fun rememberMapView(): MapView {
+    val context = LocalContext.current
+    val mapView = remember {
+        MapView(context)
+    }
+
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    DisposableEffect(lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            when (event) {
+                Lifecycle.Event.ON_CREATE -> mapView.onCreate(Bundle())
+                Lifecycle.Event.ON_START -> mapView.onStart()
+                Lifecycle.Event.ON_RESUME -> mapView.onResume()
+                Lifecycle.Event.ON_PAUSE -> mapView.onPause()
+                Lifecycle.Event.ON_STOP -> mapView.onStop()
+                Lifecycle.Event.ON_DESTROY -> mapView.onDestroy()
+                else -> throw IllegalStateException()
+            }
+        }
+
+        // Add the observer to the lifecycle
+        lifecycleOwner.lifecycle.addObserver(observer)
+
+        // When the effect leaves the Composition, remove the observer
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
+
+    return mapView
 }
