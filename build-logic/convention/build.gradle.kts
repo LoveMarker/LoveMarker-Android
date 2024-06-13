4d6bd12e1c53cba02ecd56f02a40e683001bc43f
leeeha <jxlhe46@gmail.com>
2024-06-13 17:06:09 +0900

    [feat] #3 convention 모듈의 build.gradle.kts 코드 작성

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -2,8 +2,48 @@ plugins {
     `kotlin-dsl`
 }
 
+group = "com.capstone.lovemarker.buildlogic"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
 dependencies {
     compileOnly(libs.agp)
     compileOnly(libs.kotlin.gradle.plugin)
+    compileOnly(libs.ksp.gradle.plugin)
 }
 
+gradlePlugin {
+    plugins {
+        create("android-application") {
+            id = "com.capstone.lovemarker.application"
+            implementationClass = "AndroidApplicationPlugin"
+        }
+        create("android-library") {
+            id = "com.capstone.lovemarker.library"
+            implementationClass = "AndroidLibraryPlugin"
+        }
+        create("android-kotlin") {
+            id = "com.capstone.lovemarker.kotlin"
+            implementationClass = "AndroidKotlinPlugin"
+        }
+        create("android-hilt") {
+            id = "com.capstone.lovemarker.hilt"
+            implementationClass = "AndroidHiltPlugin"
+        }
+        create("android-compose") {
+            id = "com.capstone.lovemarker.compose"
+            implementationClass = "AndroidComposePlugin"
+        }
+        create("kotlin-serialization") {
+            id = "com.capstone.lovemarker.serialization"
+            implementationClass = "KotlinSerializationPlugin"
+        }
+        create("retrofit") {
+            id = "com.capstone.lovemarker.retrofit"
+            implementationClass = "RetrofitPlugin"
+        }
+    }
+}
