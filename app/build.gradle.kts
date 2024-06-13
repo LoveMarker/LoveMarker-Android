327fe170ab711aefccd05f120fdc87dced7bf877
leeeha <jxlhe46@gmail.com>
2024-06-13 16:07:45 +0900

    [mod] #3 min sdk 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.capstone.lovemarker"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
