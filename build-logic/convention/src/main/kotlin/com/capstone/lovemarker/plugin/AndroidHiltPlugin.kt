4a0739eed6e9cefaccd3a7d8581bb1e0d33331e7
leeeha <jxlhe46@gmail.com>
2024-06-13 16:03:49 +0900

    [add] #3 android hilt plugin 추가

---
+++ build-logic/convention/src/main/kotlin/com/capstone/lovemarker/plugin/AndroidHiltPlugin.kt
@@ -0,0 +1,22 @@
+package com.capstone.lovemarker.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+
+class AndroidHiltPlugin : Plugin<Project> {
+    override fun apply(target: Project) = with(target) {
+        with(plugins) {
+            apply("com.google.devtools.ksp")
+            apply("com.google.dagger.hilt.android")
+        }
+
+        val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+        dependencies {
+            "implementation"(libs.findLibrary("hilt").get())
+            "ksp"(libs.findLibrary("hilt.android.compiler").get())
+        }
+    }
+}
