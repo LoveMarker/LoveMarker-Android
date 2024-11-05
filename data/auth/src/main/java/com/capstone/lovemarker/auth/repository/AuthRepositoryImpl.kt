d50eda7ee304ef1caa8db01f7af3a96bd786f16c
leeeha <jxlhe46@gmail.com>
2024-11-05 16:46:54 +0900

    [feat] #5 data, domain 모듈에서 로그인 코드 작성

--- data/auth/src/main/java/com/capstone/lovemarker/auth/repository/AuthRepositoryImpl.kt
+++ data/auth/src/main/java/com/capstone/lovemarker/auth/repository/AuthRepositoryImpl.kt
@@ -1,12 +1,20 @@
 package com.capstone.lovemarker.auth.repository
 
+import com.capstone.lovemarker.auth.dto.request.LoginRequest
+import com.capstone.lovemarker.auth.entity.LoginEntity
 import com.capstone.lovemarker.auth.source.AuthDataSource
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
-    private val authDataSource: AuthDataSource
+    private val authDataSource: AuthDataSource,
 ) : AuthRepository {
-    override suspend fun login() {
-        TODO("Not yet implemented")
+    override suspend fun postLogin(
+        socialToken: String,
+        provider: String,
+    ): Result<LoginEntity> = runCatching {
+        authDataSource.postLogin(
+            socialToken = socialToken,
+            requestBody = LoginRequest(provider)
+        ).data.toDomain()
     }
 }
--- domain/auth/src/main/java/com/capstone/lovemarker/auth/repository/AuthRepository.kt
+++ domain/auth/src/main/java/com/capstone/lovemarker/auth/repository/AuthRepository.kt
@@ -1,5 +1,10 @@
 package com.capstone.lovemarker.auth.repository
 
+import com.capstone.lovemarker.auth.entity.LoginEntity
+
 interface AuthRepository {
-    suspend fun login()
+    suspend fun postLogin(
+        socialToken: String,
+        provider: String
+    ): Result<LoginEntity>
 }
