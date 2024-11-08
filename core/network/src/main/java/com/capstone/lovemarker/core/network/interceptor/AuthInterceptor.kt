0745dec84b5afbcf406ade34d20e143f42cf5c55
leeeha <jxlhe46@gmail.com>
2024-11-08 14:47:26 +0900

    [feat] #9 AuthInterceptor 구현

--- core/network/src/main/java/com/capstone/lovemarker/core/network/interceptor/AuthInterceptor.kt
+++ core/network/src/main/java/com/capstone/lovemarker/core/network/interceptor/AuthInterceptor.kt
@@ -1,10 +1,30 @@
 package com.capstone.lovemarker.core.network.interceptor
 
+import com.capstone.lovemarker.core.datastore.source.UserPreferencesDataSource
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
+import javax.inject.Inject
 
-class AuthInterceptor : Interceptor {
+class AuthInterceptor @Inject constructor(
+    private val userPreferencesDataSource: UserPreferencesDataSource,
+) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        TODO("Not yet implemented")
+        val encodedToken = runBlocking {
+            "$BEARER ${userPreferencesDataSource.userData.first().accessToken}"
+        }
+
+        val originalRequest = chain.request()
+        val headerRequest = originalRequest.newBuilder()
+            .header(AUTHORIZATION, encodedToken)
+            .build()
+
+        return chain.proceed(headerRequest)
+    }
+
+    companion object {
+        private const val BEARER = "Bearer"
+        private const val AUTHORIZATION = "Authorization"
     }
 }
