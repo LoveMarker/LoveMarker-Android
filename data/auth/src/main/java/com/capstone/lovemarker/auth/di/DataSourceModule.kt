a5d3c5335a4516cffffeb3bc14f9250aa82d4fe3
leeeha <jxlhe46@gmail.com>
2024-11-09 15:52:39 +0900

    [feat] #5 service, datasource, repository에 대한 di module 코드 작성

--- data/auth/src/main/java/com/capstone/lovemarker/auth/di/DataSourceModule.kt
+++ data/auth/src/main/java/com/capstone/lovemarker/auth/di/DataSourceModule.kt
@@ -1,7 +1,7 @@
 package com.capstone.lovemarker.auth.di
 
-import com.capstone.lovemarker.auth.repository.AuthRepository
-import com.capstone.lovemarker.auth.repository.AuthRepositoryImpl
+import com.capstone.lovemarker.auth.source.AuthDataSource
+import com.capstone.lovemarker.auth.source.AuthDataSourceImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -10,8 +10,8 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class AuthModule {
+abstract class DataSourceModule {
     @Binds
     @Singleton
-    abstract fun bindAuthRepository(authRepositoryImpl: AuthRepositoryImpl): AuthRepository
+    abstract fun bindAuthDataSource(authDataSourceImpl: AuthDataSourceImpl): AuthDataSource
 }
