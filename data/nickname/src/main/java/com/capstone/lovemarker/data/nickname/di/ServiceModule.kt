174500e0314a65a91a4d9fba6632471055b9bdc6
leeeha <jxlhe46@gmail.com>
2024-11-12 01:51:59 +0900

    [feat] #8 di module 구현

--- data/nickname/src/main/java/com/capstone/lovemarker/data/nickname/di/ServiceModule.kt
+++ data/nickname/src/main/java/com/capstone/lovemarker/data/nickname/di/ServiceModule.kt
@@ -1,2 +1,19 @@
 package com.capstone.lovemarker.data.nickname.di
 
+import com.capstone.lovemarker.core.network.qualifier.AuthRequired
+import com.capstone.lovemarker.data.nickname.service.NicknameService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ServiceModule {
+    @Provides
+    @Singleton
+    fun provideNicknameService(@AuthRequired retrofit: Retrofit): NicknameService = retrofit.create()
+}
