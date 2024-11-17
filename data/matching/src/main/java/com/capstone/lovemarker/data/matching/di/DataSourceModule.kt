adc63879bb663d0cad142a78b64577094e8276e0
leeeha <jxlhe46@gmail.com>
2024-11-17 21:28:48 +0900

    [feat] #15 di module 구현

--- data/matching/src/main/java/com/capstone/lovemarker/data/matching/di/DataSourceModule.kt
+++ data/matching/src/main/java/com/capstone/lovemarker/data/matching/di/DataSourceModule.kt
@@ -1,2 +1,17 @@
 package com.capstone.lovemarker.data.matching.di
 
+import com.capstone.lovemarker.data.matching.source.MatchingDataSource
+import com.capstone.lovemarker.data.matching.source.MatchingDataSourceImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataSourceModule {
+    @Binds
+    @Singleton
+    abstract fun bindMatchingDataSource(matchingDataSourceImpl: MatchingDataSourceImpl): MatchingDataSource
+}
--- data/matching/src/main/java/com/capstone/lovemarker/data/matching/di/RepositoryModule.kt
+++ data/matching/src/main/java/com/capstone/lovemarker/data/matching/di/RepositoryModule.kt
@@ -1,2 +1,17 @@
 package com.capstone.lovemarker.data.matching.di
 
+import com.capstone.lovemarker.data.matching.repository.MatchingRepositoryImpl
+import com.capstone.lovemarker.domain.matching.repository.MatchingRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindMatchingRepository(matchingRepositoryImpl: MatchingRepositoryImpl): MatchingRepository
+}
