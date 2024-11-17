8325ff110e37791a519f8d779212c910e4429570
leeeha <jxlhe46@gmail.com>
2024-11-17 20:44:02 +0900

    [del] #23 미사용 임포트 삭제

--- core/network/src/main/java/com/capstone/lovemarker/core/network/interceptor/AuthInterceptor.kt
+++ core/network/src/main/java/com/capstone/lovemarker/core/network/interceptor/AuthInterceptor.kt
@@ -5,7 +5,6 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Response
-import timber.log.Timber
 import javax.inject.Inject
 
 class AuthInterceptor @Inject constructor(
