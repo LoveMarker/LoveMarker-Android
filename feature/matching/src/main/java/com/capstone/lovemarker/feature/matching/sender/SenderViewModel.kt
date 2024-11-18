5f97c4755a542cb79a2ccc1a4876b4f62ee0f5a6
leeeha <jxlhe46@gmail.com>
2024-11-18 18:03:50 +0900

    [feat] #15 초대 코드 생성 api 호출

--- feature/matching/src/main/java/com/capstone/lovemarker/feature/matching/sender/SenderViewModel.kt
+++ feature/matching/src/main/java/com/capstone/lovemarker/feature/matching/sender/SenderViewModel.kt
@@ -1,20 +1,63 @@
 package com.capstone.lovemarker.feature.matching.sender
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.capstone.lovemarker.domain.matching.repository.MatchingRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class SenderViewModel @Inject constructor(
-//    private val matchingRepository: MatchingRepository
+    private val matchingRepository: MatchingRepository,
 ) : ViewModel() {
+    private val _state = MutableStateFlow(SenderState())
+    val state: StateFlow<SenderState> = _state.asStateFlow()
+
     private val _sideEffect = MutableSharedFlow<SenderSideEffect>()
     val sideEffect: SharedFlow<SenderSideEffect> = _sideEffect.asSharedFlow()
 
-    fun postInvitationCode(anniversary: String) {
+    fun updateAnniversary(anniversary: String) {
+        _state.update {
+            it.copy(anniversary = anniversary)
+        }
+    }
 
+    fun updateButtonEnabled(enabled: Boolean) {
+        _state.update {
+            it.copy(buttonEnabled = enabled)
+        }
+    }
+
+    fun updateInvitationCode(invitationCode: String) {
+        _state.update {
+            it.copy(invitationCode = invitationCode)
+        }
+    }
+
+    fun updateDialogState(showDialog: Boolean) {
+        _state.update {
+            it.copy(showDialog = showDialog)
+        }
+    }
+
+    fun postInvitationCode(anniversary: String) {
+        viewModelScope.launch {
+            matchingRepository.postInvitationCode(anniversary)
+                .onSuccess { response ->
+                    Timber.d(response.invitationCode)
+                    _sideEffect.emit(SenderSideEffect.ShowShareDialog(response.invitationCode))
+                }.onFailure {
+                    _sideEffect.emit(SenderSideEffect.ShowErrorSnackbar(it))
+                }
+        }
     }
 }
