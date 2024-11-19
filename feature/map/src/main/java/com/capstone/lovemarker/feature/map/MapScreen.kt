a94d509fbf7a0c1a214286d30cd8b626484dcad9
leeeha <jxlhe46@gmail.com>
2024-11-19 10:02:30 +0900

    [ui] #25 임시 map 화면

--- feature/map/src/main/java/com/capstone/lovemarker/feature/map/MapScreen.kt
+++ feature/map/src/main/java/com/capstone/lovemarker/feature/map/MapScreen.kt
@@ -1,15 +1,41 @@
 package com.capstone.lovemarker.feature.map
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.capstone.lovemarker.core.designsystem.theme.LoveMarkerTheme
 
 @Composable
 fun MapRoute(
     innerPadding: PaddingValues,
     modifier: Modifier = Modifier,
 ) {
-    Text(text = "map screen")
+    
 }
 
+@Composable
+fun MapScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = "map screen")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun MapPreview() {
+    LoveMarkerTheme {
+        MapScreen()
+    }
+}
