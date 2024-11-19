29a89f9cfa5159cad9f912f6e815c4050cb6c1e8
leeeha <jxlhe46@gmail.com>
2024-11-19 14:13:59 +0900

    [fix] #15 커플 수락 request dto -> serializable annotation 추가

--- data/matching/src/main/java/com/capstone/lovemarker/data/matching/dto/request/CoupleJoinRequest.kt
+++ data/matching/src/main/java/com/capstone/lovemarker/data/matching/dto/request/CoupleJoinRequest.kt
@@ -1,5 +1,10 @@
 package com.capstone.lovemarker.data.matching.dto.request
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class CoupleJoinRequest(
+    @SerialName("invitationCode")
     val invitationCode: String
 )
