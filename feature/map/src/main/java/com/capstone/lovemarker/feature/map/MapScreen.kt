21ccce62a0d1dc74b6baaceb71d401c0d6b0c973
leeeha <jxlhe46@gmail.com>
2024-11-20 21:17:02 +0900

    [feat] #28 사용자의 현위치 표시

--- feature/map/src/main/java/com/capstone/lovemarker/feature/map/MapScreen.kt
+++ feature/map/src/main/java/com/capstone/lovemarker/feature/map/MapScreen.kt
@@ -1,51 +1,102 @@
 package com.capstone.lovemarker.feature.map
 
+import android.content.pm.PackageManager
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.capstone.lovemarker.core.designsystem.theme.LoveMarkerTheme
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.CameraPositionState
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
+import timber.log.Timber
 
 @Composable
 fun MapRoute(
     innerPadding: PaddingValues,
-    modifier: Modifier = Modifier,
+    viewModel: MapViewModel = viewModel()
 ) {
-    MapScreen()
+    val cameraPositionState = rememberCameraPositionState()
+    val userLocation by viewModel.userLocation
+
+    RequestLocationPermission(viewModel)
+
+    MapScreen(
+        innerPadding = innerPadding,
+        cameraPositionState = cameraPositionState,
+        userLocation = userLocation
+    )
+}
+
+@Composable
+fun RequestLocationPermission(
+    viewModel: MapViewModel
+) {
+    val context = LocalContext.current
+    val fusedLocationClient = remember { LocationServices.getFusedLocationProviderClient(context) }
+
+    val permissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            viewModel.getUserLocation(context, fusedLocationClient)
+        } else {
+            Timber.e("Location permission was denied by the user.")
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        when (PackageManager.PERMISSION_GRANTED) {
+            ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                viewModel.getUserLocation(context, fusedLocationClient)
+            }
+            else -> {
+                permissionLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        }
+    }
 }
 
 @Composable
 fun MapScreen(
-    modifier: Modifier = Modifier
+    innerPadding: PaddingValues,
+    cameraPositionState: CameraPositionState,
+    userLocation: LatLng?
 ) {
-    val singapore = LatLng(1.35, 103.87)
-    val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(singapore, 10f)
-    }
-
     GoogleMap(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding),
         cameraPositionState = cameraPositionState
     ) {
-        Marker(
-            state = MarkerState(position = singapore),
-            title = "Singapore",
-            snippet = "Marker in Singapore"
-        )
+        userLocation?.let { position ->
+            Marker(
+                state = MarkerState(position = position),
+                title = "Your Location",
+                snippet = "This is where you are currently located."
+            )
+            cameraPositionState.position = CameraPosition.fromLatLngZoom(position, 18f)
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 private fun MapPreview() {
-    LoveMarkerTheme {
-        MapScreen()
-    }
+    LoveMarkerTheme {}
 }
