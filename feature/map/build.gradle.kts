81006a51510211eeb571fa3d79dd1e488326c8b5
leeeha <jxlhe46@gmail.com>
2024-11-20 20:54:40 +0900

    [add] #28 feature.map 모듈에 구글 맵 관련 의존성 추가

--- feature/map/build.gradle.kts
+++ feature/map/build.gradle.kts
@@ -10,4 +10,7 @@ android {
 
 dependencies {
     implementation(libs.compose.maps)
+    implementation(libs.google.maps)
+    implementation(libs.google.places)
+    implementation(libs.google.location)
 }
