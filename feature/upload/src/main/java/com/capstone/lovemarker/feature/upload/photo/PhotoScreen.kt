d29c0f85206278ace11f1509508303c854085bc5
leeeha <jxlhe46@gmail.com>
2024-11-21 15:26:14 +0900

    [ui] #36 photo screen 배경 색상 변경

--- feature/upload/src/main/java/com/capstone/lovemarker/feature/upload/photo/PhotoScreen.kt
+++ feature/upload/src/main/java/com/capstone/lovemarker/feature/upload/photo/PhotoScreen.kt
@@ -23,10 +23,13 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -66,7 +69,9 @@ fun PhotoScreen(
     onNextButtonClick: () -> Unit,
 ) {
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
     ) {
         CenterAlignedTopAppBar(
             navigationIcon = {
@@ -82,7 +87,10 @@ fun PhotoScreen(
                     text = stringResource(R.string.upload_title),
                     style = LoveMarkerTheme.typography.body16B,
                 )
-            }
+            },
+            colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                containerColor = Color.White
+            )
         )
         Spacer(modifier = Modifier.padding(top = 14.dp))
         Row(
