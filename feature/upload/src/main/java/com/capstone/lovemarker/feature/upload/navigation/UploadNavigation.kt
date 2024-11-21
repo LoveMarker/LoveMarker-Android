4a5c20a041248911355f71cec8baa486919359b2
leeeha <jxlhe46@gmail.com>
2024-11-21 12:08:54 +0900

    [feat] #36 upload navigation 함수 정의

--- feature/upload/src/main/java/com/capstone/lovemarker/feature/upload/navigation/UploadNavigation.kt
+++ feature/upload/src/main/java/com/capstone/lovemarker/feature/upload/navigation/UploadNavigation.kt
@@ -1,2 +1,32 @@
 package com.capstone.lovemarker.feature.upload.navigation
 
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.capstone.lovemarker.core.navigation.UploadRoute
+
+fun NavController.navigateToPhoto() {
+    navigate(UploadRoute.Photo)
+}
+
+fun NavController.navigateToContent() {
+    navigate(UploadRoute.Content)
+}
+
+fun NavController.navigateToPlaceSearch() {
+    navigate(UploadRoute.PlaceSearch)
+}
+
+fun NavGraphBuilder.uploadNavGraph(
+    navigateUp: () -> Unit,
+) {
+    composable<UploadRoute.Photo> {
+
+    }
+    composable<UploadRoute.Content> {
+
+    }
+    composable<UploadRoute.PlaceSearch> {
+
+    }
+}
