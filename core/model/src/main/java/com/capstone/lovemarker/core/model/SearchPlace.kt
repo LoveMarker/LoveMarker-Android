a4b9f2f2a9b951b6e776c6f55a95dbd350b12b5d
leeeha <jxlhe46@gmail.com>
2024-11-25 00:49:05 +0900

    [del] #42 SearchPlace 클래스에서 기본값 삭제

--- core/model/src/main/java/com/capstone/lovemarker/core/model/SearchPlace.kt
+++ core/model/src/main/java/com/capstone/lovemarker/core/model/SearchPlace.kt
@@ -4,8 +4,8 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class SearchPlace(
-    val name: String = "",
-    val address: String = "",
-    val latitude: Double = 0.0,
-    val longitude: Double = 0.0
+    val name: String,
+    val address: String,
+    val latitude: Double,
+    val longitude: Double
 )
