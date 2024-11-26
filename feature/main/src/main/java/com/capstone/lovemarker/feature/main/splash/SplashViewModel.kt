db6fe7eea0f0978f44cef6f6cf2cd691dd4da393
leeeha <jxlhe46@gmail.com>
2024-11-26 17:28:14 +0900

    [chore] #44 splash -> map 네비게이션에서 발생하는 이슈 메모

--- feature/main/src/main/java/com/capstone/lovemarker/feature/main/splash/SplashViewModel.kt
+++ feature/main/src/main/java/com/capstone/lovemarker/feature/main/splash/SplashViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.capstone.lovemarker.domain.auth.repository.AuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
@@ -15,9 +16,13 @@ class SplashViewModel @Inject constructor(
     private val authRepository: AuthRepository,
 ) : ViewModel() {
     private val _splashSideEffect = MutableSharedFlow<SplashSideEffect>()
-    val splashSideEffect: SharedFlow<SplashSideEffect> get() = _splashSideEffect.asSharedFlow()
+    val splashSideEffect: SharedFlow<SplashSideEffect> = _splashSideEffect.asSharedFlow()
 
-    fun checkAutoLogin() {
+    init {
+        checkAutoLogin()
+    }
+
+    private fun checkAutoLogin() {
         viewModelScope.launch {
             if (authRepository.isAutoLoginEnabled()) {
                 _splashSideEffect.emit(SplashSideEffect.NavigateToMap)
