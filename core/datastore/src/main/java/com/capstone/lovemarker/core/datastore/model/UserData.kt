7c7d5f85a5df2d6aa92d9297f6186a1ca278ca90
leeeha <jxlhe46@gmail.com>
2024-11-27 23:08:16 +0900

    [feat] #50 유저 데이터스토어에 닉네임 속성 추가

--- core/datastore/src/main/java/com/capstone/lovemarker/core/datastore/model/UserData.kt
+++ core/datastore/src/main/java/com/capstone/lovemarker/core/datastore/model/UserData.kt
@@ -3,5 +3,6 @@ package com.capstone.lovemarker.core.datastore.model
 data class UserData(
     val accessToken: String,
     val refreshToken: String,
-    val autoLoginConfigured: Boolean
+    val autoLoginConfigured: Boolean,
+    val nickname: String,
 )
