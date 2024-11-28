f348f101fa71744ed4f2cb55e7a71d58aae135af
leeeha <jxlhe46@gmail.com>
2024-11-28 21:55:31 +0900

    [del] #44 미사용 임포트 삭제

--- feature/main/src/main/java/com/capstone/lovemarker/feature/main/component/MainBottomBar.kt
+++ feature/main/src/main/java/com/capstone/lovemarker/feature/main/component/MainBottomBar.kt
@@ -5,33 +5,16 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideIn
 import androidx.compose.animation.slideOut
-import androidx.compose.foundation.background
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBarsPadding
-import androidx.compose.foundation.selection.selectable
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.dp
 import com.capstone.lovemarker.core.designsystem.theme.LoveMarkerTheme
 import com.capstone.lovemarker.feature.main.navigation.MainTab
 import kotlinx.collections.immutable.PersistentList
@@ -52,7 +35,7 @@ fun MainBottomBar(
         NavigationBar(
             containerColor = LoveMarkerTheme.colorScheme.surfaceContainer
         ) {
-            tabs.forEachIndexed { idx, tab ->
+            tabs.forEach { tab ->
                 MainBottomBarItem(
                     tab = tab,
                     selected = tab == currentTab,
